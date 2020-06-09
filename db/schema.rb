cba30959aba888eaac6988a936efba9c4d4d6f8f
jhell85 <jhell85@gmail.com>
2020-06-09 12:11:00 -0700

    add token autorization for api requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_163746) do
+ActiveRecord::Schema.define(version: 2020_06_09_171304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "quotes", force: :cascade do |t|
     t.string "content"
     t.string "author"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
